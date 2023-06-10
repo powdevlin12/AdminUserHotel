8e2aa6950f9c02058a646cd829895ef529af6388
Sichain <info@sichain.vn>
2023-06-10 15:32:29 +0700

    hotel page

---
+++ src/data/hotels.js
@@ -0,0 +1,95 @@
+export const hotelGrid = [
+  { type: 'checkbox', width: '50' },
+  {
+    headerText: 'Name',
+    width: '200',
+    field: 'Name',
+    textAlign: 'Center',
+  },
+  {
+    headerText: 'Star',
+    width: '100',
+    field: 'Star',
+    textAlign: 'Center',
+  },
+  {
+    headerText: 'Description',
+    width: '200',
+    field: 'Description',
+    textAlign: 'Center',
+  },
+  {
+    headerText: 'Address',
+    width: '300',
+    field: 'Address',
+    textAlign: 'Center',
+  },
+  {
+    headerText: 'Approve',
+    field: 'approve',
+    width: '150',
+    displayAsCheckBox: true,
+    textAlign: 'Center',
+    editType: 'booleanedit',
+  },
+];
+
+export const hotelsData = [
+  {
+    Name: 'Khách sạn 69',
+    Star: '4.5',
+    Description: 'Tốt trong tầm giá',
+    Address: '97 Man Thiện',
+    approve: true,
+  },
+  {
+    Name: 'Khách sạn 96',
+    Star: '4.3',
+    Description: 'Tốt trong tầm giá',
+    Address: '96 Man Thiện',
+    approve: false,
+  },
+  {
+    Name: 'Khách sạn 6996',
+    Star: '4.1',
+    Description: 'Tốt trong tầm giá',
+    Address: '97 Man Thiện',
+    approve: true,
+  },
+  {
+    Name: 'Khách sạn 14',
+    Star: '2.5',
+    Description: 'Tốt trong tầm giá',
+    Address: '97 Man Thiện',
+    approve: false,
+  },
+  {
+    Name: 'Khách sạn 69',
+    Star: '4.5',
+    Description: 'Tốt trong tầm giá',
+    Address: '97 Man Thiện',
+    approve: false,
+  },
+  {
+    Name: 'Khách sạn 96',
+    Star: '4.3',
+    Description: 'Tốt trong tầm giá',
+    Address: '96 Man Thiện',
+    approve: false,
+  },
+  {
+    Name: 'Khách sạn 6996',
+    Star: '4.1',
+    Description: 'Tốt trong tầm giá',
+    Address: '97 Man Thiện',
+    approve: false,
+  },
+  {
+    Name: 'Khách sạn 14',
+    Star: '2.5',
+    Description: 'Tốt trong tầm giá',
+    Address: '97 Man Thiện',
+    approve: false,
+  },
+];
+
---
+++ src/pages/Hotels.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page, Toolbar, Edit, Sort } from '@syncfusion/ej2-react-grids';
+
+import { Header } from '../components';
+import { hotelGrid, hotelsData } from '../data/hotels';
+
+const Hotel = () => {
+  const toolbarOptions = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+  const editing = { allowDeleting: true, allowEditing: true, allowAdding: true };
+  // delete action
+  const actionBegin = (args) => {
+    if (args.requestType === 'delete') { // triggers while deleting the record
+      console.log('actionBegin delete triggers');
+      console.log(args.data);
+    }
+    if (args.requestType === 'save') { // triggers while adding the record
+      console.log('actionBegin triggers');
+      console.log(args.data);
+    }
+  };
+
+  const actionComplete = (args) => {
+    if (args.requestType === 'save') { // triggers when the record was added
+      console.log('actionComplete triggers');
+      console.log(args.data);
+    }
+    if (args.requestType === 'delete') { // triggers when the record was deleted
+      console.log('actionComplete triggers');
+      console.log(args.data);
+    }
+  };
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Hotels" />
+      <GridComponent
+        dataSource={hotelsData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5, pageSize: 8 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+        actionBegin={actionBegin}
+        actionComplete={actionComplete}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {hotelGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Search, Page, Toolbar, Edit, Sort]} />
+
+      </GridComponent>
+    </div>
+  );
+};
+export default Hotel;
