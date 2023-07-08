50d3ea03f94243879df86d903985b4f137c991e7
tranthudat2k1 <thudatdl123@gmail.com>
2023-07-08 16:01:19 +0700

    clean code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { Employees } from './pages';
 import './App.css';
 import Login from './pages/auth/login';
 
@@ -12,13 +9,9 @@ import { useStateContext } from './contexts/ContextProvider';
 import Hotel from './pages/Hotels';
 import { HotelProvider } from './contexts/HotelProvider';
 import PrivateRoute from './components/PrivateRoute';
-import { get } from './utils/localstorage';
-import { useAuthContext } from './contexts/AuthProvider';
-// import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
-  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+  const { currentMode } = useStateContext();
   return (
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
